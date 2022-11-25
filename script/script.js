fccb277c7868391e392d1cde9212f0e80e67ccb8
Fatih Arapoglu <f3tt4n@gmail.com>
2022-11-25 16:44:05 +0300

    add remove button and fix checkbox

--- script/script.js
+++ script/script.js
@@ -3,7 +3,6 @@ const formDOM = document.querySelector("#form");
 formDOM.addEventListener('submit', (event) => {
     event.preventDefault();
     addBookToLibrary() ;
-
     });
 
 
@@ -16,7 +15,7 @@ const readDOM = document.getElementById("read");
 
 // empty array which will be filled with book objects
 let myLibrary = [];
-console.log (myLibrary);
+console.log(myLibrary);
 
 
 // constructor
@@ -26,7 +25,7 @@ function Book (title, author, page, read) {
     this.page = page;
     this.read = read;
     this.uniqueID = Date.now()
-    this.info = function () {
+    this.info = function() {
         return `${title} by ${author}, ${page} pages, Finished? -> ${read}`;
     };
     console.log (this.info());
@@ -42,29 +41,52 @@ function addBookToLibrary() {
     let newBook = new Book (title, author, page, read);
     myLibrary.push (newBook);
     console.log (myLibrary);
-    displayArray(myLibrary.length - 1);
+    displayArray();
 }
 
 
 // display array function
 const displayDOM = document.getElementById("display");
 
-function displayArray(e) {
-
-    displayDOM.innerHTML += `
-    <div id="${myLibrary[e].uniqueID}" class="card flex content-center items-center flex-col">
-        <span class="material-symbols-outlined pb-5">menu_book</span>
-        <h5 class="bookTitle">${myLibrary[e].title}</h5>
-        <p class="bookAuthor">${myLibrary[e].author}</p>
-        <p class="bookAuthor">${myLibrary[e].page} pages</p>
-        <div>
-        <input type="checkbox" id="read" name="read" value="true">
-        <label class="ml-1 text-sm font-medium text-gray-900" for="read"> Finished it? </label>
-        </div>
-    </div>
-    `
+function displayArray() {
+    displayDOM.innerHTML = ""
+    for (let key of myLibrary){
+        if (key.read) {        
+        displayDOM.innerHTML += `
+            <div class="card flex content-center items-center flex-col">
+                <span class="material-symbols-outlined pb-5">menu_book</span>
+                <h5 class="bookTitle">${key.title}</h5>
+                <p class="bookAuthor">${key.author}</p>
+                <p class="bookAuthor">${key.page} pages</p>
+                <div>
+                    <input type="checkbox" id="read" name="read" value="true" checked>
+                    <label class="ml-1 text-sm font-medium text-gray-900" for="read"> Finished it? </label>
+                </div>
+                <button id="${key.uniqueID}">Remove</button>
+            </div>
+            `
+        }
+        else {
+        displayDOM.innerHTML += `
+            <div class="card flex content-center items-center flex-col">
+                <span class="material-symbols-outlined pb-5">menu_book</span>
+                <h5 class="bookTitle">${key.title}</h5>
+                <p class="bookAuthor">${key.author}</p>
+                <p class="bookAuthor">${key.page} pages</p>
+                <div>
+                    <input type="checkbox" id="read" name="read" value="true">
+                    <label class="ml-1 text-sm font-medium text-gray-900" for="read"> Finished it? </label>
+                </div>
+                <button id="${key.uniqueID}">Remove</button>
+            </div>
+            `
+        }
+    }
 };
 
-function removeFromArray () {
-    
+function removeFromArray() {
+    const removeButtons = displayDOM.querySelectorAll("button");
+    removeButtons.forEach(button => button.addEventListener('click', event => {
+        console.log ( event)
+    }))
 }
