d94c6d6df201e94965761f4cf9e579cac5c2dca8
Fatih Arapoglu <f3tt4n@gmail.com>
2023-02-07 23:57:48 +0300

    add firebase log in started

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import {
+    getAuth,
+    onAuthStateChanged,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+} from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+
 // form event listener
 const formDOM = document.querySelector("#form");
 formDOM.addEventListener("submit", (event) => {
@@ -26,8 +35,8 @@ class Book {
     }
     info = () => {
         return `${this.title} by ${this.author}, ${this.page} pages, Finished? -> ${this.read}`;
-    }
-};
+    };
+}
 
 // submit event function which adds the books to the array
 function addBookToLibrary() {
@@ -35,12 +44,12 @@ function addBookToLibrary() {
     let author = authorDOM.value;
     let page = pageDOM.value;
     let read = readDOM.checked;
-    let newBook = new Book (title, author, page, read);
+    let newBook = new Book(title, author, page, read);
     myLibrary.push(newBook);
     displayArray();
     clearForm();
     defaultModalDOM.classList.add("hidden");
-};
+}
 
 // creates DOM cards with innerHTML
 const displayDOM = document.getElementById("display");
@@ -54,25 +63,29 @@ function displayArray() {
                 <p class="bookAuthor">${book.author}</p>
                 <p class="bookAuthor">${book.page} pages</p>
                 <div>
-                    <input type="checkbox" id="read" name="read" value="true" ${book.read ? "checked" : "unchecked"}>
+                    <input type="checkbox" id="read" name="read" value="true" ${
+                        book.read ? "checked" : "unchecked"
+                    }>
                     <label class="ml-1 text-sm text-gray-900" for="read"> Finished it? </label>
                 </div>
                 <button class="btn-remove" id="${book.uniqueID}">Remove</button>
             </div>
-            `
+            `;
     }
-};
+}
 
 // remove array function with eventListeners of created buttons
 function removeFromArray() {
     const removeButtons = displayDOM.querySelectorAll("button");
-    removeButtons.forEach(button => button.addEventListener("click", event => {
-        let index = myLibrary.findIndex(book => book.uniqueID == event.target.id); // finds the index of matched id
-        myLibrary.splice(index, 1); // deletes from array
-        displayArray(); // refreshes the display
-        return removeFromArray(); // returns itself so it continues to listen the events after removing one item.
-    }))
-};
+    removeButtons.forEach((button) =>
+        button.addEventListener("click", (event) => {
+            let index = myLibrary.findIndex((book) => book.uniqueID == event.target.id); // finds the index of matched id
+            myLibrary.splice(index, 1); // deletes from array
+            displayArray(); // refreshes the display
+            return removeFromArray(); // returns itself so it continues to listen the events after removing one item.
+        })
+    );
+}
 
 // clear function
 function clearForm() {
@@ -80,7 +93,7 @@ function clearForm() {
     authorDOM.value = "";
     pageDOM.value = "";
     readDOM.checked = "";
-};
+}
 
 // modal settings
 const openModalDOM = document.getElementById("open-modal");
@@ -92,3 +105,60 @@ openModalDOM.onclick = () => {
 closeModalDOM.onclick = () => {
     defaultModalDOM.classList.add("hidden");
 };
+
+// firebase
+const signGoogleDOM = document.getElementById("google-login");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCiXcNl8XwAPCOk1C833nt1aVdc4ZiJdT4",
+    authDomain: "library-68253.firebaseapp.com",
+    projectId: "library-68253",
+    storageBucket: "library-68253.appspot.com",
+    messagingSenderId: "999698004532",
+    appId: "1:999698004532:web:c64c709dcfe8670e71a861",
+};
+
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const provider = new GoogleAuthProvider();
+
+signGoogleDOM.addEventListener("click", () => {
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+            console.log(user);
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+            console.log(errorMessage);
+        });
+});
+
+onAuthStateChanged(auth, (user) => {
+    if (user !== null) {
+        console.log("logged in");
+    } else {
+        console.log("not logged in");
+    }
+});
+
+signOut(auth)
+    .then(() => {
+        // Sign-out successful.
+    })
+    .catch((error) => {
+        // An error happened.
+    });
